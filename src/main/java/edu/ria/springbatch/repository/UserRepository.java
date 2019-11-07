4b9a74ba742beb0a4a1682853a470534daf53ef9
riadipa <riadipa@gmail.com>
2019-11-07 18:24:56 +0100

    Added multiple databases as source database and writing into target database

--- src/main/java/edu/ria/springbatch/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.ria.springbatch.repository;
-
-import edu.ria.springbatch.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
